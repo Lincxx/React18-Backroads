c3091c0d8bee4bfadab03d7788442e9b74d14438
lincxx <bossc@BOSSCO>
2023-07-19 15:04:51 -0400

    Finsihed project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import About from './components/About'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Services from './components/Services'
+import Tours from './components/Tours'
+import Footer from './components/Footer'
+
 function App() {
-  return <h1>Backroads app</h1>
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Footer.js
@@ -0,0 +1,39 @@
+import { pageLinks, socialLinks } from '../data'
+function Footer() {
+  return (
+    <div>
+      <footer className="section footer">
+        <ul className="footer-links">
+          {pageLinks.map((pageLink) => {
+            const { href, text, id } = pageLink
+            return (
+              <li key={id}>
+                <a href={href} className="footer-link">
+                  {text}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+
+        <ul className="footer-icons">
+          {socialLinks.map((socialLink) => {
+            const { id, href, icon } = socialLink
+            return (
+              <li key={id}>
+                <a href={href} target="_blank" className="footer-icon">
+                  <i className={icon}></i>
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+        <p className="copyright">
+          copyright &copy; Backroads travel tours company
+          <span id="date"></span> all rights reserved
+        </p>
+      </footer>
+    </div>
+  )
+}
+export default Footer
